cf566e2321fac95449de72cc59af72e29a23f45d
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-07-13 13:56:43 +0700

    Menambahkan Semua File dari project BeautyCare | | Halaman Register

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Akses Login -- Rolee ---
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/admin/dashboard', function () {
+            return view('admin.dashboard');
+        })->name('admin.dashboard');
+    });
+    Route::middleware(['role:kasir'])->group(function () {
+        Route::get('/kasir/dashboard', function () {
+            return view('kasir.dashboard');
+        })->name('kasir.dashboard');
+    });
+    Route::middleware(['role:beautycian'])->group(function () {
+        Route::get('/beautycian/dashboard', function () {
+            return view('beautycian.dashboard');
+        })->name('beautycian.dashboard');
+    });
+});
+
+require __DIR__ . '/auth.php';
