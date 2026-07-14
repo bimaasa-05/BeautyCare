f6f02030ceef8e54d40f118da261971cb7008ffc
dulsxcod <xiaomijatibarang40@gmail.com>
2026-07-14 14:17:14 +0700

    menambahkan halaman pengelola pelanggan atau menambahkan folder, file indexnya, dan controllernyabeserta route nya

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PelangganController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -24,6 +25,7 @@ Route::middleware('auth')->group(function () {
         })->name('admin.dashboard');
 
         Route::get('/admin/users', [UserController::class, 'index'])->name('admin.user.index');
+        Route::get('/admin/pelanggan', [PelangganController::class, 'index'])->name('admin.pelanggan.index');
     });
     Route::middleware(['role:kasir'])->group(function () {
         Route::get('/kasir/dashboard', function () {
