d7e0abcdb2d6d6c2fd5f3d9d9e336b1217a7171c
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-07-15 10:41:20 +0700

    Memperbaiki Routing dan Merapikannya

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,13 @@ Route::get('/', function () {
     return view('landing.index');
 })->name('home');
 
-Route::get('/pelanggan/dashboard', function () {
-    return view('pelanggan.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    //Akses Login -- Rolee ---
+    //Akses Login -- Rolee --- Admin
     Route::middleware(['role:admin'])->group(function () {
         Route::get('/admin/dashboard', function () {
             return view('admin.dashboard');
@@ -27,20 +24,32 @@ Route::middleware('auth')->group(function () {
 
         Route::get('/admin/users', [UserController::class, 'index'])->name('admin.user.index');
         Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.user.create');
-        });
-        Route::get('/admin/pelanggan', [PelangganController::class, 'index'])->name('admin.pelanggan.index');
-        Route::get('/admin/beautician', [BeauticianController::class, 'index'])->name('admin.beautician.index');
     });
+    Route::get('/admin/pelanggan', [PelangganController::class, 'index'])->name('admin.pelanggan.index');
+    Route::get('/admin/beautician', [BeauticianController::class, 'index'])->name('admin.beautician.index');
+
+
+    //-------------------------------------------------
+    //Route Kasir
     Route::middleware(['role:kasir'])->group(function () {
         Route::get('/kasir/dashboard', function () {
             return view('kasir.dashboard');
         })->name('kasir.dashboard');
     });
+    //--------------------------------------------------
+    //Route BeautyCian
     Route::middleware(['role:beautycian'])->group(function () {
         Route::get('/beautycian/dashboard', function () {
             return view('beautycian.dashboard');
         })->name('beautycian.dashboard');
     });
+    //--------------------------------------------------
+    //Route Pelangggan
+    Route::get('/pelanggan/dashboard', function () {
+        return view('pelanggan.dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+    //--------------------------------------------------
 });
+
 
 require __DIR__ . '/auth.php';
