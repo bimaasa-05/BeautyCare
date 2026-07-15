36f4e8bbc9807eb661bd03c131371694d35fa592
dulsxcod <xiaomijatibarang40@gmail.com>
2026-07-15 11:50:25 +0700

    menambahka isinya

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class AdminUserController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.user.index');
+    }
+
+    public function create()
+    {
+        return view('admin.user.create');
+    }
 }
