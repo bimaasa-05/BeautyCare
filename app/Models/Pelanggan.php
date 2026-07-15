cf48d8bcb06881b098691f15090dd3e6d66610f2
dulsxcod <xiaomijatibarang40@gmail.com>
2026-07-15 14:21:40 +0700

    menambahkan model pelanggan

---
+++ app/Models/Pelanggan.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pelanggan extends Model
+{
+    protected $table = 'pelanggan';
+    protected $primaryKey = 'id_pelanggan';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nm_pelanggan',
+        'no_hp',
+        'email',
+        'alamat',
+        'id_member',
+        'catatan_alergi',
+        'foto',
+    ];
+}
