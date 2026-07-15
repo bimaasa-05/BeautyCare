91c174b66869fbca8c4252e6a2790c38f54720b8
dulsxcod <xiaomijatibarang40@gmail.com>
2026-07-15 11:49:57 +0700

    menambahka isinya

--- app/Http/Controllers/AdminPelangganController.php
+++ app/Http/Controllers/AdminPelangganController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class AdminPelangganController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.pelanggan.index');
+    }
 }
