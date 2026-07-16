873bed05b3fb087513a3d4e023fc2880515d525c
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-07-16 10:31:57 +0700

    Mengubah dan menbahkan kan route kasir

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\KasirPelangganController;
 use App\Http\Controllers\BeauticianController;
 use Illuminate\Support\Facades\Route;
 
@@ -35,6 +36,30 @@ Route::middleware('auth')->group(function () {
         Route::get('/kasir/dashboard', function () {
             return view('kasir.dashboard');
         })->name('kasir.dashboard');
+
+        Route::get('/kasir/pelanggan', [KasirPelangganController::class, 'index'])->name('kasir.pelanggan.index');
+        Route::get('/kasir/pelanggan/create', [KasirPelangganController::class, 'create'])->name('kasir.pelanggan.create');
+        Route::post('/kasir/pelanggan', [KasirPelangganController::class, 'store'])->name('kasir.pelanggan.store');
+        Route::get('/kasir/pelanggan/{id}', [KasirPelangganController::class, 'show'])->name('kasir.pelanggan.show');
+        Route::get('/kasir/pelanggan/{id}/edit', [KasirPelangganController::class, 'edit'])->name('kasir.pelanggan.edit');
+        Route::put('/kasir/pelanggan/{id}', [KasirPelangganController::class, 'update'])->name('kasir.pelanggan.update');
+        Route::delete('/kasir/pelanggan/{id}', [KasirPelangganController::class, 'destroy'])->name('kasir.pelanggan.destroy');
+
+        Route::get('/kasir/transaksi', function () {
+            return view('kasir.transaksi.index');
+        })->name('kasir.transaksi.index');
+        Route::get('/kasir/checkin', function () {
+            return view('kasir.checkin.index');
+        })->name('kasir.checkin.index');
+        Route::get('/kasir/pembayaran', function () {
+            return view('kasir.pembayaran.index');
+        })->name('kasir.pembayaran.index');
+        Route::get('/kasir/invoice', function () {
+            return view('kasir.invoice.index');
+        })->name('kasir.invoice.index');
+        Route::get('/kasir/riwayat-transaksi', function () {
+            return view('kasir.riwayat-transaksi.index');
+        })->name('kasir.riwayat-transaksi.index');
     });
     //--------------------------------------------------
     //Route BeautyCian
@@ -48,7 +73,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/pelanggan/dashboard', function () {
         return view('pelanggan.dashboard');
     })->middleware(['auth', 'verified'])->name('dashboard');
-    //--------------------------------------------------
+    
 });
 
 
