01d7145332a3b4c5f9844dd04c3872d0dc656507
Lorantz07 <gilasihhh0@gmail.com>
2026-07-16 12:38:04 +0700

    Memperbaiki Route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\KasirPelangganController;
 use App\Http\Controllers\BeauticianController;
+use App\Http\Controllers\BeautycianController;
+use App\Http\Controllers\AdminUserController;
+use App\Http\Controllers\AdminPelangganController;
+use App\Http\Controllers\AdminBeautycianController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -22,11 +27,25 @@ Route::middleware('auth')->group(function () {
             return view('admin.dashboard');
         })->name('admin.dashboard');
 
-        Route::get('/admin/users', [UserController::class, 'index'])->name('admin.user.index');
-        Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.user.create');
+        Route::get('/admin/users', [AdminUserController::class, 'index'])->name('admin.user.index');
+        Route::get('/admin/users/create', [AdminUserController::class, 'create'])->name('admin.user.create');
+        Route::post('/admin/users', [AdminUserController::class, 'store'])->name('admin.user.store');
+        Route::get('/admin/users/{user}/edit', [AdminUserController::class, 'edit'])->name('admin.user.edit');
+        Route::put('/admin/users/{user}', [AdminUserController::class, 'update'])->name('admin.user.update');
+        Route::delete('/admin/users/{user}', [AdminUserController::class, 'destroy'])->name('admin.user.destroy');
+        Route::get('/admin/pelanggan', [AdminPelangganController::class, 'index'])->name('admin.pelanggan.index');
+        Route::get('/admin/pelanggan/create', [AdminPelangganController::class, 'create'])->name('admin.pelanggan.create');
+        Route::post('/admin/pelanggan', [AdminPelangganController::class, 'store'])->name('admin.pelanggan.store');
+        Route::get('/admin/pelanggan/{pelanggan}/edit', [AdminPelangganController::class, 'edit'])->name('admin.pelanggan.edit');
+        Route::put('/admin/pelanggan/{pelanggan}', [AdminPelangganController::class, 'update'])->name('admin.pelanggan.update');
+        Route::delete('/admin/pelanggan/{pelanggan}', [AdminPelangganController::class, 'destroy'])->name('admin.pelanggan.destroy');
+        Route::get('/admin/beautician', [AdminBeautycianController::class, 'index'])->name('admin.beautician.index');
+        Route::get('/admin/beautician/create', [AdminBeautycianController::class, 'create'])->name('admin.beautician.create');
+        Route::post('/admin/beautician', [AdminBeautycianController::class, 'store'])->name('admin.beautician.store');
+        Route::get('/admin/beautician/{beautician}/edit', [AdminBeautycianController::class, 'edit'])->name('admin.beautician.edit');
+        Route::put('/admin/beautician/{beautician}', [AdminBeautycianController::class, 'update'])->name('admin.beautician.update');
+        Route::delete('/admin/beautician/{beautician}', [AdminBeautycianController::class, 'destroy'])->name('admin.beautician.destroy');
     });
-    Route::get('/admin/pelanggan', [PelangganController::class, 'index'])->name('admin.pelanggan.index');
-    Route::get('/admin/beautician', [BeauticianController::class, 'index'])->name('admin.beautician.index');
 
 
     //-------------------------------------------------
@@ -35,6 +54,30 @@ Route::middleware('auth')->group(function () {
         Route::get('/kasir/dashboard', function () {
             return view('kasir.dashboard');
         })->name('kasir.dashboard');
+
+        Route::get('/kasir/pelanggan', [KasirPelangganController::class, 'index'])->name('kasir.pelanggan.index');
+        Route::get('/kasir/pelanggan/create', [KasirPelangganController::class, 'create'])->name('kasir.pelanggan.create');
+        Route::post('/kasir/pelanggan', [KasirPelangganController::class, 'store'])->name('kasir.pelanggan.store');
+        Route::get('/kasir/pelanggan/{id}', [KasirPelangganController::class, 'show'])->name('kasir.pelanggan.show');
+        Route::get('/kasir/pelanggan/{id}/edit', [KasirPelangganController::class, 'edit'])->name('kasir.pelanggan.edit');
+        Route::put('/kasir/pelanggan/{id}', [KasirPelangganController::class, 'update'])->name('kasir.pelanggan.update');
+        Route::delete('/kasir/pelanggan/{id}', [KasirPelangganController::class, 'destroy'])->name('kasir.pelanggan.destroy');
+
+        Route::get('/kasir/transaksi', function () {
+            return view('kasir.transaksi.index');
+        })->name('kasir.transaksi.index');
+        Route::get('/kasir/checkin', function () {
+            return view('kasir.checkin.index');
+        })->name('kasir.checkin.index');
+        Route::get('/kasir/pembayaran', function () {
+            return view('kasir.pembayaran.index');
+        })->name('kasir.pembayaran.index');
+        Route::get('/kasir/invoice', function () {
+            return view('kasir.invoice.index');
+        })->name('kasir.invoice.index');
+        Route::get('/kasir/riwayat-transaksi', function () {
+            return view('kasir.riwayat-transaksi.index');
+        })->name('kasir.riwayat-transaksi.index');
     });
     //--------------------------------------------------
     //Route BeautyCian
@@ -48,6 +91,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/pelanggan/dashboard', function () {
         return view('pelanggan.dashboard');
     })->middleware(['auth', 'verified'])->name('dashboard');
+     Route::get('pelanggan/booking', [PelangganController::class, 'index'])->name('pelanggan.booking');
+     
     //--------------------------------------------------
 });
 
