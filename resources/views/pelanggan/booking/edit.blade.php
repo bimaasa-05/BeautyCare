eca4a927105e3676c9801422fba51cf8e7bb9ba8
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-07-20 16:18:27 +0700

    Memperbaiki Halaman Booking/edit

--- resources/views/pelanggan/booking/edit.blade.php
+++ resources/views/pelanggan/booking/edit.blade.php
@@ -517,10 +517,6 @@
 </head>
 
 <body>
-    <div class="page-loader">
-        <div class="loader-spinner"></div>
-    </div>
-
     <div class="dashboard-layout">
         @include('layouts.sidebar')
 
