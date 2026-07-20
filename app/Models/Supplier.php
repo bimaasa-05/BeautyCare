479d6720b2a5b0d1b6334ce52de5b5a1ca099a38
dulsxcod <xiaomijatibarang40@gmail.com>
2026-07-20 10:51:45 +0700

    Menghubungkan dengan database

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     protected $table = 'supplier';
-    protected $primaryKey = 'id_produk';
+    protected $primaryKey = 'id_supplier';
     public $timestamps = false;
 
     protected $fillable = [
