4245007806655240cf9d54d4e7a353a923d60a72
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-07-20 14:51:02 +0700

    Memperbaiki layanan/edit

--- resources/views/admin/layanan/edit.blade.php
+++ resources/views/admin/layanan/edit.blade.php
@@ -84,10 +84,6 @@
 
 <body>
     <!-- Page Loader -->
-    <div class="page-loader">
-        <div class="loader-spinner"></div>
-    </div>
-
     <div class="dashboard-layout">
         @include('layouts.sidebar')
 
