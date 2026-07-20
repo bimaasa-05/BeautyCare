17effb4de5d8c928d61f34299ac364a49ef84f2a
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-07-20 16:25:57 +0700

    Memperbaiki Halaman profile/

--- resources/views/pelanggan/profile/index.blade.php
+++ resources/views/pelanggan/profile/index.blade.php
@@ -473,10 +473,6 @@
 </head>
 
 <body>
-    <div class="page-loader">
-        <div class="loader-spinner"></div>
-    </div>
-
     <div class="dashboard-layout">
         @include('layouts.sidebar')
 
