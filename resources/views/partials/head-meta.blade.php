9de6bf400e50330bbcea1569321273d444c63f28
dulsxcod <xiaomijatibarang40@gmail.com>
2026-07-23 16:28:43 +0700

    menambahkan icon untuk semua halaman yang ada di role pada header google

---
+++ resources/views/partials/head-meta.blade.php
@@ -0,0 +1,7 @@
+<meta name="description"
+    content="@yield('meta_description', 'BeautyCare - Aplikasi Manajemen Bisnis Kecantikan untuk Salon, Spa, Nail Art, Barbershop, dan Skincare.')">
+<meta name="keywords" content="beautycare, manajemen salon, software kecantikan, booking online, POS salon">
+<meta name="author" content="BeautyCare">
+
+<link rel="icon" type="image/svg+xml" href="{{ asset('assets/images/logo/favicon.svg') }}">
+<link rel="alternate icon" href="{{ asset('assets/images/logo/favicon.svg') }}">
