fcd28617609302f89ca695567dbd2f73b112dad1
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-07-24 17:01:51 +0700

    Memperbaiki KasirPelangganController dan mengoptimalkannya

--- app/Http/Controllers/KasirPelangganController.php
+++ app/Http/Controllers/KasirPelangganController.php
@@ -13,7 +13,7 @@ class KasirPelangganController extends Controller
         $search = $request->keyword;
 
 
-        $TotalPelanggan = Pelanggan::with('membership')->paginate(0);
+        $TotalPelanggan = Pelanggan::count();
         $pelanggan = Pelanggan::when($search, function ($query, $search) {
             return $query->where('nm_pelanggan', 'like', "%{$search}%")->orwhere('no_hp', 'like', "%{$search}%")->orwhere('email', 'like', "%{$search}%");
         })->orderBy('id_pelanggan', 'desc')->paginate(10);
@@ -45,10 +45,8 @@ class KasirPelangganController extends Controller
             'alamat' => $request->alamat,
             'id_member' => $request->id_member,
             'catatan_alergi' => $request->catatan_alergi,
-            'foto' => $request->nullable,
         ];
         if ($request->hasFile('foto')) {
-            // Menyimpan file ke folder 'uploads/pelanggan' di disk public
             $data['foto'] = $request->file('foto')->store('uploads/pelanggan', 'public');
         }
         Pelanggan::create($data);
@@ -76,7 +74,7 @@ class KasirPelangganController extends Controller
         //
         $request->validate([
             'nm_pelanggan' => 'required|string|max:255',
-            'email' => 'required|email|unique:pelanggan,email',
+            'email' => 'required|email|unique:pelanggan,email,' . $id . ',id_pelanggan',
             'no_hp' => 'required|string|max:15',
             'alamat' => 'required',
             'id_member' => 'nullable|string',
@@ -84,16 +82,18 @@ class KasirPelangganController extends Controller
             'foto' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
 
-
-        Pelanggan::where('id_pelanggan', $id)->update([
+        $data = [
             'nm_pelanggan' => $request->nm_pelanggan,
             'email' => $request->email,
             'no_hp' => $request->no_hp,
             'alamat' => $request->alamat,
             'id_member' => $request->id_member,
             'catatan_alergi' => $request->catatan_alergi,
-            'foto' => $request->nullable,
-        ]);
+        ];
+        if ($request->hasFile('foto')) {
+            $data['foto'] = $request->file('foto')->store('uploads/pelanggan', 'public');
+        }
+        Pelanggan::where('id_pelanggan', $id)->update($data);
         buatNotif(auth()->id(), 'Pelanggan Diperbarui', 'Data pelanggan berhasil diperbarui', 'Lainnya', route('kasir.pelanggan.index'));
 
         return redirect('kasir/pelanggan')->with('message', 'data berhasil di Diupdate');
