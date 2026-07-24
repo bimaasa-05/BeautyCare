bd13dbc0054da088b1c345b811f6aac12693aa26
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-07-24 17:03:17 +0700

    Memperbaiki Table transaksi

--- database/migrations/2026_07_14_124108_transaksi.php
+++ database/migrations/2026_07_14_124108_transaksi.php
@@ -21,8 +21,8 @@ return new class extends Migration
             $table->date('tanggal');
             $table->decimal('subtotal', 12);
             $table->decimal('diskon', 12);
-            $table->decimal('pajak', 12)->default(0);
-            $table->decimal('total', 12)->default(0);
+            $table->decimal('pajak', 12);
+            $table->decimal('total', 12);
             $table->enum('metode_byr', ['Tunai', 'Transfer', 'Debit', 'E-Wallet']);
             $table->decimal('dibayar', 12);
             $table->decimal('kembali', 12);
