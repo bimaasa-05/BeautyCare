82aa18b8b2b7cf11449364ef1aa35ad78d649e87
Lorantz07 <gilasihhh0@gmail.com>
2026-07-26 11:28:29 +0700

    menghapus loading yang ada pada halaman kerangjang

--- resources/views/pelanggan/keranjang/index.blade.php
+++ resources/views/pelanggan/keranjang/index.blade.php
@@ -1089,10 +1089,6 @@
 </head>
 
 <body>
-    <div class="page-loader">
-        <div class="loader-spinner"></div>
-    </div>
-
     <div class="dashboard-layout">
         @include('layouts.sidebar')
 
