594cfbfc93b9191e593b4a00549b978872aa3fc7
dulsxcod <xiaomijatibarang40@gmail.com>
2026-08-01 13:40:28 +0700

    menambahkan bahasa indo

--- lang/id/pagination.php
+++ lang/id/pagination.php
@@ -5,4 +5,9 @@ return [
     'previous' => '&laquo; Sebelumnya',
     'next' => 'Berikutnya &raquo;',
 
+    'showing' => 'Menampilkan',
+    'to' => 'hingga',
+    'of' => 'dari',
+    'results' => 'hasil',
+
 ];
