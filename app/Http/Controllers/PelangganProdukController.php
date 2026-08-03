09e6294991a7cf4fc6feb01666421883c93bdf13
Lorantz07 <gilasihhh0@gmail.com>
2026-08-03 14:53:31 +0700

    menambahkan public funtion show, untuk mengarah ke detail produk

--- app/Http/Controllers/PelangganProdukController.php
+++ app/Http/Controllers/PelangganProdukController.php
@@ -38,4 +38,31 @@ class PelangganProdukController extends Controller
 
         return view('pelanggan.produk.index', compact('produks', 'favoritProdukIds'));
     }
+
+    public function show($id)
+    {
+        $produk = Produk::with('kategori')
+            ->where('status', 'Tersedia')
+            ->findOrFail($id);
+
+        $favoritCount = FavoritProduk::where('id_produk', $produk->id_produk)->count();
+        $isFavorit = FavoritProduk::where('id_user', auth()->id())
+            ->where('id_produk', $produk->id_produk)
+            ->exists();
+
+        $beliCount = DetailTransaksi::join('transaksi', 'transaksi.id_transaksi', '=', 'detail_transaksi.id_transaksi')
+            ->where('detail_transaksi.jenis', 'Produk')
+            ->where('detail_transaksi.id_item', $produk->id_produk)
+            ->where('transaksi.status', 'Lunas')
+            ->sum('detail_transaksi.qty');
+
+        $produkLainnya = Produk::with('kategori')
+            ->where('status', 'Tersedia')
+            ->where('id_produk', '!=', $produk->id_produk)
+            ->where('id_kategori_produk', $produk->id_kategori_produk)
+            ->limit(4)
+            ->get();
+
+        return view('pelanggan.produk.detail', compact('produk', 'favoritCount', 'isFavorit', 'beliCount', 'produkLainnya'));
+    }
 }
