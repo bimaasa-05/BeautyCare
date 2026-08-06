1b6e59f3c9417c7e32bb043ad7f72e7bfd2dcaa6
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-08-06 20:49:13 +0700

    Memperbaiki Pelanggan Treatment Controller dan Mengoptimalkannya di bagian cetak pdf

--- app/Http/Controllers/PelangganTreatmentController.php
+++ app/Http/Controllers/PelangganTreatmentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Booking;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 
 class PelangganTreatmentController extends Controller
@@ -32,6 +33,17 @@ class PelangganTreatmentController extends Controller
             ->firstOrFail();
 
         return view('pelanggan.treatment.detail', compact('booking'));
+    }
+
+    public function pdf($id)
+    {
+        $booking = Booking::with(['detail.layanan', 'karyawan', 'transaksi', 'pelanggan', 'riwayatTreatment'])
+            ->where('id_booking', $id)
+            ->where('id_pelanggan', auth()->id())
+            ->firstOrFail();
+
+        $pdf = Pdf::loadView('pelanggan.treatment.pdf', compact('booking'));
+        return $pdf->download('Detail-Treatment-BK' . str_pad($booking->id_booking, 3, '0', STR_PAD_LEFT) . '.pdf');
     }
 
 }
