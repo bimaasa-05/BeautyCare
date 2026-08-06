0910854ad48c3de0511ce741d73fa7faea8fea29
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-08-06 16:08:48 +0700

    Memperbaiki sidebar kasir dan mengoptimalakn nya juga

--- resources/views/layouts/sidebar-kasir.blade.php
+++ resources/views/layouts/sidebar-kasir.blade.php
@@ -197,7 +197,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     document.querySelectorAll('#sidebar .nav-item').forEach(function(item) {
         item.addEventListener('click', function() {
-            if (window.innerWidth <= 768) closeSidebar();
+            if (window.innerWidth <= 1024) closeSidebar();
         });
     });
 });
