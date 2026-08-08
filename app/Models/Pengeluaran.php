77f34bd90b4fa02be72837c24dd666ab3173a524
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-08-08 07:10:58 +0700

    Menambahkan Model Pengeluaran

---
+++ app/Models/Pengeluaran.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pengeluaran extends Model
+{
+    protected $table = 'pengeluaran';
+    protected $primaryKey = 'id_pengeluaran';
+
+    protected $fillable = [
+        'tanggal',
+        'kategori',
+        'keterangan',
+        'nominal',
+        'id_user',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+}
