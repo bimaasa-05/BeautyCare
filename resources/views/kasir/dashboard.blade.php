674c59bc385d64a2c551c88de5c516bf15fcd92f
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-08-09 22:51:20 +0700

    Memperbaiki Kasir Dashboar index dan Merpikan FrontEndnya juga ya

--- resources/views/kasir/dashboard.blade.php
+++ resources/views/kasir/dashboard.blade.php
@@ -99,7 +99,7 @@
             background: #F9A8D4;
         }
 
-        .chart-card .chart-actions select.dropdown-pink {
+        select.dropdown-pink {
             appearance: none;
             background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='14' height='14' viewBox='0 0 24 24' fill='none' stroke='%23EC4899' stroke-width='2.5' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpolyline points='6 9 12 15 18 9'%3E%3C/polyline%3E%3C/svg%3E");
             background-repeat: no-repeat;
@@ -117,14 +117,14 @@
             cursor: pointer;
         }
 
-        .chart-card .chart-actions select.dropdown-pink:hover {
+        select.dropdown-pink:hover {
             border-color: #FCE7F3;
             background-color: #fff;
             color: #EC4899;
             box-shadow: none;
         }
 
-        .chart-card .chart-actions select.dropdown-pink:focus {
+        select.dropdown-pink:focus {
             border-color: #FCE7F3;
             box-shadow: none;
             outline: none;
