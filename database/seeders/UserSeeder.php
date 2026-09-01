9558905f8d59e81cb4fbdcfb19650760738d0634
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-09-01 13:29:41 +0700

    Memperbaiki UserSEeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,36 +13,40 @@ class UserSeeder extends Seeder
             'nama' => 'Admin',
             'email' => 'a@gmail.com',
             'no_hp' => '081234567891',
-            'password' => '123456',
+            'password' => 'Beautycare01',
             'role' => 'admin',
             'status' => 'aktif',
+            'email_verified_at' => now(),
         ]);
 
         User::create([
             'nama' => 'Kasir',
             'email' => 'k@gmail.com',
             'no_hp' => '081234567892',
-            'password' => '123456',
+            'password' => 'Beautycare02',
             'role' => 'kasir',
             'status' => 'aktif',
+            'email_verified_at' => now(),
         ]);
 
         User::create([
             'nama' => 'Beautycian',
             'email' => 'b@gmail.com',
             'no_hp' => '081234567893',
-            'password' => '123456',
+            'password' => 'Beautycare03',
             'role' => 'beautycian',
             'status' => 'aktif',
+            'email_verified_at' => now(),
         ]);
 
         User::create([
             'nama' => 'Pelanggan',
             'email' => 'p@gmail.com',
             'no_hp' => '081234567894',
-            'password' => '123456',
+            'password' => 'Beautycare04',
             'role' => 'pelanggan',
             'status' => 'aktif',
+            'email_verified_at' => now(),
         ]);
     }
 }
